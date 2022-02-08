61d533a96d160e84480e9a76dfc711d6ecc288fe
Jeffrey <jeffrey.rodriigues@gmail.com>
2022-02-07 22:18:51 -0300

    Dark Mode Finished

--- src/App.jsx
+++ src/App.jsx
@@ -2,16 +2,22 @@ import About from "./components/about/About.jsx";
 import Intro from "./components/intro/Intro.jsx";
 import ProductList from "./components/productList/ProductList.jsx";
 import Contact from "./components/contact/Contact.jsx";
-import Toogle from "./components/toogle/Toogle.jsx";
+import { useContext } from "react";
+import { ThemeContext } from "./context";
+import Toggle from "./components/toggle/Toggle.jsx";
 
 const App = () => {
-  return <div>
-    <Toogle />
-    <Intro />
-    <About />
-    <ProductList />
-    <Contact />
-  </div>
+  const theme = useContext(ThemeContext)
+  const darkMode = theme.state.darkMode;
+  return (
+    <div style={{ backgroundColor: darkMode ? "#222" : "white", color: darkMode && "white" }}>
+      <Toggle />
+      <Intro />
+      <About />
+      <ProductList />
+      <Contact />
+    </div>
+  );
 };
 
 
--- src/components/contact/Contact.jsx
+++ src/components/contact/Contact.jsx
@@ -2,12 +2,15 @@ import "./contact.css"
 import Phone from "../../img/phone.png"
 import Email from "../../img/email.png"
 import Adress from "../../img/address.png"
-import { useRef, useState } from "react";
+import { useContext, useRef, useState } from "react";
 import emailjs from "emailjs-com";
+import { ThemeContext } from "../../context";
 
 const Contact = () => {
   const formRef = useRef();
   const [done, setDone] = useState(false)
+  const theme = useContext(ThemeContext)
+  const darkMode = theme.state.darkMode;
 
   const handleSubmit = (e) => {
     e.preventDefault();
@@ -28,7 +31,7 @@ const Contact = () => {
       <div className="c-wrapper">
         <div className="c-left">
           <h1 className="c-title">
-            Let's discuss yout project
+            Contatos
           </h1>
           <div className="c-info">
             <div className="c-info-item">
@@ -47,13 +50,13 @@ const Contact = () => {
         </div>
         <div className="c-right">
           <p className="c-desc">
-            <b>What's your story? </b> sit amet consectetur adipisicing elit. Aut mollitia possimus, consequuntur labore a blanditiis delectus sunt qui.
+            <b>Qual é a sua hitória? </b> sit amet consectetur adipisicing elit. Aut mollitia possimus, consequuntur labore a blanditiis delectus sunt qui.
           </p>
           <form ref={formRef} onSubmit={handleSubmit}>
-            <input type="text" placeholder="Name" name="user_name" />
-            <input type="text" placeholder="Subject" name="user_subject" />
-            <input type="text" placeholder="Email" name="user_email" />
-            <textarea rows="5" placeholder="Message" name="message"></textarea>
+            <input style={{ backgroundColor: darkMode && "#333" }} type="text" placeholder="Name" name="user_name" />
+            <input style={{ backgroundColor: darkMode && "#333" }} type="text" placeholder="Subject" name="user_subject" />
+            <input style={{ backgroundColor: darkMode && "#333" }} type="text" placeholder="Email" name="user_email" />
+            <textarea style={{ backgroundColor: darkMode && "#333" }} rows="5" placeholder="Message" name="message"></textarea>
             <button>Enviar</button>
             {done && "Obrigado...!"}
           </form>
